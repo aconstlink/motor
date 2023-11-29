8b2cf5fa0d12bd335194430decc9c6e3ff95ba29
aconstlink <aconstlink@gmail.com>
2023-11-29 12:07:33 +0100

    added insertion_sort + some typedefs

--- std/insertion_sort.hpp
+++ std/insertion_sort.hpp
@@ -1,32 +1,33 @@
 
 #pragma once
 
-#include "typedefs.h"
+#include <motor/core/types.hpp>
 
-#include <natus/core/macros/typedef.h>
 #include <functional>
 #include <array>
 
-namespace natus
+namespace motor
 {
-    namespace ntd
+    namespace mstd
     {
+        using namespace motor::core::types ;
+
         template< typename T >
         class insertion_sort
         {
-            natus_this_typedefs( insertion_sort< T > ) ;
+            using this_t = insertion_sort< T > ;
 
         public:
 
             typedef std::function< void_t ( size_t const a, size_t const b ) > swap_funk_t ;
 
             
-            static void_t in_range( size_t const a, size_t const b, std::vector< T > & points, swap_funk_t funk = [](size_t const a, size_t const b){} )
+            static void_t in_range( size_t const a, size_t const b, motor::vector< T > & points, swap_funk_t funk = [](size_t const a, size_t const b){} )
             {
                 return this_t::the_sort( a, b, points, funk ) ;
             }
 
-            static void_t for_all( std::vector< T > & points, swap_funk_t funk = [](size_t const a, size_t const b){} )
+            static void_t for_all( motor::vector< T > & points, swap_funk_t funk = [](size_t const a, size_t const b){} )
             {
                 return this_t::the_sort( 0, points.size(), points, funk ) ;
             }
@@ -39,7 +40,7 @@ namespace natus
 
         private:
 
-            static void_t the_sort( size_t const a, size_t const b, std::vector< T > & points, swap_funk_t funk = [](size_t const a, size_t const b){} )
+            static void_t the_sort( size_t const a, size_t const b, motor::vector< T > & points, swap_funk_t funk = [](size_t const a, size_t const b){} )
             {
                 for( size_t i=a+1; i<b; ++i )
                 {
