a2960f296d9cb280617dc8713a1f951dab2abc15
aconstlink <aconstlink@gmail.com>
2023-11-29 13:17:16 +0100

    some fixes in the string split function

--- std/string_split.hpp
+++ std/string_split.hpp
@@ -5,33 +5,36 @@
 
 namespace motor
 {    
-    struct string_ops
+    namespace mstd
     {
-        static size_t split( motor::string_cref_t string_in, char deli,
-            motor::vector<motor::string_t>& split_out )
+        struct string_ops
         {
-            size_t num_found = 0 ;
-            size_t found_at = 0 ;
-            size_t begin = 0 ;
-
-            do
+            static size_t split( motor::string_cref_t string_in, char deli,
+                motor::vector<motor::string_t>& split_out )
             {
-                found_at = string_in.find_first_of( deli, begin ) ;
-                if( begin < found_at )
+                size_t num_found = 0 ;
+                size_t found_at = 0 ;
+                size_t begin = 0 ;
+
+                do
                 {
-                    split_out.push_back( string_in.substr( begin, found_at - begin ) ) ;
-                    ++num_found ;
+                    found_at = string_in.find_first_of( deli, begin ) ;
+                    if( begin < found_at )
+                    {
+                        split_out.push_back( string_in.substr( begin, found_at - begin ) ) ;
+                        ++num_found ;
+                    }
+                    begin = found_at + 1 ;
+
+                } while( found_at != std::string::npos ) ;
+
+                if( num_found > 0 && split_out.back().size() == 0 )
+                {
+                    split_out.resize( split_out.size() - 1 ) ;
                 }
-                begin = found_at + 1 ;
 
-            } while( found_at != std::string::npos ) ;
-
-            if( num_found > 0 && split_out.back().size() == 0 )
-            {
-                split_out.resize( split_out.size() - 1 ) ;
+                return num_found ;
             }
-
-            return num_found ;
-        }
-    };
+        };
+    }
 }
