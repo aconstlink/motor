13f39362db74ed67a786b12924ee56760ea5b262
aconstlink <aconstlink@gmail.com>
2023-12-11 12:08:54 +0100

    :art: added default typedefs

--- application/window/iwindow.h
+++ application/window/iwindow.h
@@ -16,5 +16,6 @@ namespace motor
             // check for user messages and pass further to window implementation.
             virtual void_t check_for_messages( void_t ) noexcept = 0 ;
         } ;
+        motor_typedef( iwindow ) ;
     }
 }
