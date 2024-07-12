c3af35a4c3a614f68433bea80629ece9e6fdc672
Alex Link <aconstlink@gmail.com>
2024-07-12 02:10:24 +0200

    retried with export symbols and now it is working

--- externals/nelarius/imnodes_config.h
+++ externals/nelarius/imnodes_config.h
@@ -1,2 +1,11 @@
 
 #pragma once
+
+#if defined( MOTOR_BUILD_DLL )
+#if defined( MOTOR_IMNODES_EXPORT )
+#define IMNODES_API __declspec( dllexport )
+#else
+#define IMNODES_API __declspec( dllimport )
+#endif
+#else
+#endif
