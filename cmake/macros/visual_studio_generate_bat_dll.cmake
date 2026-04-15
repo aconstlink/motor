0f1af4476cb66c0dac94546afe33b71108f39dd0
Alex Link <aconstlink@gmail.com>
2026-04-15 22:34:38 +0200

    changed a path

--- cmake/macros/visual_studio_generate_bat_dll.cmake
+++ cmake/macros/visual_studio_generate_bat_dll.cmake
@@ -13,7 +13,7 @@ if( MSVC_IDE )
 # for later usage: rename solution file and remove the old one. Need to be called after generation of solution file
   #file( RENAME ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.sln ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.DONT_START_THIS.sln )
   #file( REMOVE ${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}.sln )
-  file( WRITE "${CMAKE_BINARY_DIR}/START_HERE.bat" "set PATH=%PATH%;${motor_bindir_debug};${motor_bindir_debug}" "\n")
+  file( WRITE "${CMAKE_BINARY_DIR}/START_HERE.bat" "set PATH=%PATH%;${motor_bindir_debug};${motor_bindir_release}" "\n")
   file( APPEND "${CMAKE_BINARY_DIR}/START_HERE.bat" "call ${CMAKE_PROJECT_NAME}.sln" )
   
 endif()
