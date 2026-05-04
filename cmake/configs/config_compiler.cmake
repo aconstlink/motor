7e555dff0a20d1a5715c9a8d95b3a98308288d00
aconstlink <aconstlink@gmail.com>
2026-05-04 09:34:40 +0200

    :art: more on C++ Standard for MSVC and GNU :green_heart:

--- cmake/configs/config_compiler.cmake
+++ cmake/configs/config_compiler.cmake
@@ -11,18 +11,24 @@ set( MOTOR_COMPILER_CONFIGURED FALSE )
 set( THIS_TARGET ct_motor_compiler )
 add_library( ${THIS_TARGET} INTERFACE )
 
-# just set the cxx version to 11 for now
+# 
+# 2026 motor is updated to c++ 20.
+#
 # it need to be used in the target property CXX_STANDARD
-set( MOTOR_CXX_STANDARD 17 )
+set( MOTOR_CXX_STANDARD 20 )
+set( CMAKE_CXX_STANDARD_REQUIRED 20 )
 
 #message( ${CMAKE_CXX_COMPILER_ID} )
  
 # Microsoft compiler
 set( MOTOR_COMPILER_MSC OFF )
+
+# @TODO do we still need this?
 set( MOTOR_COMPILER_MSC_14 OFF ) # vs 2015
 set( MOTOR_COMPILER_MSC_15 OFF ) # vs 2017
 set( MOTOR_COMPILER_MSC_16 OFF ) # vs 2019
 set( MOTOR_COMPILER_MSC_17 OFF ) # vs 2022
+set( MOTOR_COMPILER_MSC_20 OFF ) # cs 2026
 
 # Gnu compiler (GCC, GCC-C++)
 set( MOTOR_COMPILER_GNU OFF )
@@ -32,35 +38,34 @@ if( CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" )
 
     set( MOTOR_COMPILER_MSC ON )
     
-    if( MSVC_VERSION EQUAL 1900 )
-      set( MOTOR_COMPILER_MSC_14 on )
-      set( MOTOR_CXX_STANDARD 11 )
-    elseif( MSVC_VERSION GREATER 1909 AND MSVC_VERSION LESS 1920 )
-      set( MOTOR_COMPILER_MSC_15 on )
-      set( MOTOR_CXX_STANDARD 14 )
-    elseif( MSVC_VERSION GREATER 1919 AND MSVC_VERSION LESS 1930 )
-      set( MOTOR_COMPILER_MSC_16 on )
-      set( MOTOR_CXX_STANDARD 17 )
-    elseif( MSVC_VERSION GREATER 1929 AND MSVC_VERSION LESS 1938 )
-      set( MOTOR_COMPILER_MSC_17 on )
-      set( MOTOR_CXX_STANDARD 17 )
+    if(  MSVC_VERSION GREATER 1929 )
+      message( STATUS "[motor compiler] : MSVC Version > 1929" )
+      set( MOTOR_COMPILER_MSC_20 on )
     else()
-      message( STATUS "MSVC Version > 1938" )
-      set( MOTOR_COMPILER_MSC_17 on )
-      set( MOTOR_CXX_STANDARD 17 )
+      message( FATAL_ERROR "MSVC compiler not supported anymore. Need" 
+        ${MOTOR_CXX_VERSION} "C++ Standard")
     endif()
 
     target_compile_definitions( ${THIS_TARGET} INTERFACE -DMOTOR_COMPILER_MSC )
+    set_property(TARGET ${THIS_TARGET} PROPERTY CXX_STANDARD ${MOTOR_CXX_STANDARD} )
 
 elseif( CMAKE_CXX_COMPILER_ID STREQUAL "GNU" )
 
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 13)
+        message(FATAL_ERROR "GCC version must be at least 13! Found ${CMAKE_CXX_COMPILER_VERSION}")
+    endif()
+
+    message( STATUS "[motor compiler] : GNU GCC Version OK" )
+
     set( MOTOR_COMPILER_GNU ON )
     target_compile_definitions( ${THIS_TARGET} INTERFACE -DMOTOR_COMPILER_GNU )
+    set_property(TARGET ${THIS_TARGET} PROPERTY CXX_STANDARD ${MOTOR_CXX_STANDARD} )
 
 elseif( CMAKE_CXX_COMPILER_ID STREQUAL "Clang" )
 
     set( MOTOR_COMPILER_CLANG ON )
     target_compile_definitions( ${THIS_TARGET} INTERFACE -DMOTOR_COMPILER_CLANG )
+    set_property(TARGET ${THIS_TARGET} PROPERTY CXX_STANDARD ${MOTOR_CXX_STANDARD} )
 
 else()
 
@@ -86,7 +91,10 @@ elseif( MOTOR_COMPILER_CLANG )
 
 endif()
 
+
 set( CMAKE_CXX_STANDARD ${MOTOR_CXX_STANDARD} )
+set( CMAKE_CXX_EXTENSIONS OFF )
+
 set( MOTOR_COMPILER_CONFIGURED TRUE )
 
 export( TARGETS ${THIS_TARGET}
