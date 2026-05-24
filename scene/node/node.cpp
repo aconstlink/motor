32e69b716e1e5f788a00754ab653a3fda6ec4061
aconstlink <aconstlink@gmail.com>
2026-05-24 09:49:27 +0200

    :hammer: mainly formating :art: + added component callbacks

--- scene/node/node.cpp
+++ scene/node/node.cpp
@@ -3,74 +3,76 @@
 #include "../component/icomponent.h"
 #include "../visitor/ivisitor.h"
 
-using namespace motor::scene ;
+using namespace motor::scene;
 
 //*******************************************************************
-node::traverser::traverser( node_ptr_t begin ) noexcept :_traverse( begin )
-{
-}
+node::traverser::traverser( node_ptr_t begin ) noexcept : _traverse( begin ) {}
 
 //*******************************************************************
 void_t node::traverser::apply( motor::scene::ivisitor_ptr_t v ) noexcept
 {
-    v->on_start() ;
-    _traverse->apply( v ) ;
-    v->on_finish() ;
+    v->on_start();
+    _traverse->apply( v );
+    v->on_finish();
 }
 
 //*******************************************************************
-node::derived_apply::derived_apply( node_ptr_t begin ) noexcept :_traverse( begin )
-{
-}
+node::derived_apply::derived_apply( node_ptr_t begin ) noexcept : _traverse( begin ) {}
 
 //*******************************************************************
 motor::scene::result node::derived_apply::apply( motor::scene::ivisitor_ptr_t v ) noexcept
 {
-    return _traverse->apply( v ) ;
+    return _traverse->apply( v );
 }
 
 //*******************************************************************
-node::node( void_t ) noexcept
-{}
+node::node( void_t ) noexcept {}
 
 //*******************************************************************
-node::node( node::this_ptr_t parent_ptr ) noexcept  : _parent(parent_ptr)
-{}
+node::node( node::this_ptr_t parent_ptr ) noexcept : _parent( parent_ptr ) {}
 
 //*******************************************************************
 node::node( this_rref_t rhv ) noexcept
 {
-    _parent = motor::move( rhv._parent ) ;
-    _components = std::move( rhv._components ) ;
+    _parent = motor::move( rhv._parent );
+
+    rhv.for_each_component( [ & ]( motor::scene::icomponent_ptr_t comp ) {
+        comp->moved_to( this ) ;
+    } );
+    _components = std::move( rhv._components );
 }
 
 //*******************************************************************
-node::this_ref_t node::operator = ( this_rref_t rhv ) noexcept
+node::this_ref_t node::operator=( this_rref_t rhv ) noexcept
 {
-    _parent = motor::move( rhv._parent ) ;
-    _components = std::move( rhv._components ) ;
-    return *this ;
+    _parent = motor::move( rhv._parent );
+    rhv.for_each_component( [ & ]( motor::scene::icomponent_ptr_t comp ) {
+        comp->moved_to( this ) ;
+    } );
+    _components = std::move( rhv._components );
+    return *this;
 }
 
 //*******************************************************************
 node::~node( void_t ) noexcept
 {
-    for( auto * comp : _components ) 
+    for( auto * comp : _components )
     {
-        motor::memory::release_ptr( comp ) ;
+        comp->detach() ;
+        motor::memory::release_ptr( comp );
     }
 }
 
 //*******************************************************************
 node::this_ptr_t node::borrow_parent( void_t ) noexcept
 {
-    return _parent ;
+    return _parent;
 }
 
 //*******************************************************************
 node::this_ptr_t node::set_parent( node::this_ptr_t parent_ptr ) noexcept
 {
-    auto old_parent = _parent ;
-    _parent = parent_ptr ;
-    return old_parent ;
+    auto old_parent = _parent;
+    _parent = parent_ptr;
+    return old_parent;
 }
--- scene/node/node.h
+++ scene/node/node.h
@@ -12,162 +12,186 @@
 
 namespace motor
 {
-    namespace scene
+namespace scene
+{
+class MOTOR_SCENE_API node : public motor::scene::ivisitable
+{
+    motor_this_typedefs( node );
+
+  private:
+
+    /// every node has a parent node
+    /// if no parent exists, it is the root
+    this_ptr_t _parent = nullptr;
+
+    motor::vector< motor::scene::icomponent_mtr_t > _components;
+
+  public:
+
+    node( void_t ) noexcept;
+    node( this_ptr_t ) noexcept;
+    node( this_rref_t ) noexcept;
+    node( this_cref_t ) = delete;
+    virtual ~node( void_t ) noexcept;
+
+    this_ref_t operator=( this_cref_t ) = delete;
+    this_ref_t operator=( this_rref_t ) noexcept;
+
+  public: // parent
+
+    /// returns the parent node.
+    this_ptr_t borrow_parent( void_t ) noexcept;
+
+    /// allows to set the parent of this node while
+    /// returning the old one.
+    this_ptr_t set_parent( this_ptr_t ) noexcept;
+
+    // virtual motor::scene::result replace( this_ptr_t which_ptr, this_ptr_t with_ptr ) = 0 ;
+    // virtual motor::scene::result detach( this_ptr_t which_ptr ) = 0 ;
+
+  public: // component
+
+    template < typename T >
+    bool_t has_component( void_t ) const noexcept
     {
-        class MOTOR_SCENE_API node : public motor::scene::ivisitable
-        {
-            motor_this_typedefs( node ) ;
-
-        private:
-
-            /// every node has a parent node
-            /// if no parent exists, it is the root
-            this_ptr_t _parent = nullptr ;
-
-            motor::vector< motor::scene::icomponent_mtr_t > _components ;
-
-        public:
-
-            node( void_t ) noexcept ;
-            node( this_ptr_t ) noexcept ;
-            node( this_rref_t ) noexcept ;
-            node( this_cref_t ) = delete ;
-            virtual ~node( void_t ) noexcept ;
-
-            this_ref_t operator = ( this_cref_t ) = delete ;
-            this_ref_t operator = ( this_rref_t ) noexcept ;
-
-        public: // parent
-
-            /// returns the parent node.
-            this_ptr_t borrow_parent( void_t ) noexcept ;
-
-            /// allows to set the parent of this node while
-            /// returning the old one.
-            this_ptr_t set_parent( this_ptr_t ) noexcept ;
-
-            //virtual motor::scene::result replace( this_ptr_t which_ptr, this_ptr_t with_ptr ) = 0 ;
-            //virtual motor::scene::result detach( this_ptr_t which_ptr ) = 0 ;
-
-        public: // component 
-
-            template< typename T >
-            bool_t has_component( void_t ) const noexcept
-            {
-                return this_t::borrow_component<T>() != nullptr ;
-            }
-
-            template< typename T >
-            T * borrow_component( void_t ) const noexcept
-            {
-                for ( auto * comp : _components )
-                {
-                    if ( auto * ret = dynamic_cast<T*>( comp ); ret != nullptr )
-                    {
-                        return ret ;
-                    }
-                }
-                return nullptr ;
-            }
-
-            template< typename T >
-            bool_t borrow_component( T * & ret ) const noexcept
-            {
-                for ( auto * comp : _components )
-                {
-                    if ( ret = dynamic_cast<T *>( comp ); ret != nullptr )
-                    {
-                        return true ;
-                    }
-                }
-                return false ;
-            }
-
-            template< typename T >
-            bool_t add_component( motor::core::mtr_safe<T> comp ) noexcept
-            {
-                auto iter = std::find_if( _components.begin(), _components.end(), 
-                [&] ( motor::scene::icomponent_ptr_t comp_in ) 
-                {
-                    return comp_in == comp || (dynamic_cast<T *>( comp_in ) != nullptr) ;
-                } ) ;
-
-                if ( iter != _components.end() ) 
-                {
-                    motor::release( comp ) ;
-                    return false ;
-                }
-
-                comp->attached_to( this ) ;
-                _components.emplace_back( motor::move( comp ) ) ;                
-
-                return true ;
-            }
-
-            using for_each_comp_funk_t = std::function< void_t ( motor::scene::icomponent_ptr_t ) > ;
-            void_t for_each_component( for_each_comp_funk_t funk ) noexcept 
-            {
-                for( auto * comp : _components )
-                {
-                    funk( comp ) ;
-                }
-            }
-
-        public:
-
-            // helps traversing the tree
-            // use this call to access the apply() function.
-            class MOTOR_SCENE_API traverser
-            {
-            private:
-
-                node_ptr_t _traverse ;
-
-            public:
-
-                traverser( node_ptr_t begin ) noexcept ;
-                void_t apply( motor::scene::ivisitor_ptr_t ) noexcept ;
-            };
-
-            // for nodes to be called
-            class derived_apply
-            {
-            private:
-
-                node_ptr_t _traverse ;
-
-            public:
-
-                derived_apply( node_ptr_t begin ) noexcept ;
-                motor::scene::result apply( motor::scene::ivisitor_ptr_t ) noexcept ;
-            };
-
-        protected:
-
-            virtual motor::scene::result apply( motor::scene::ivisitor_ptr_t ) noexcept = 0 ;
-
-        public:
-
-            virtual bool_t inputs( motor::wire::inputs_out_t ) noexcept { return false ; }
-
-            template< typename T >
-            static bool_t is_of( this_ptr_t ptr ) noexcept
-            {
-                return dynamic_cast<T*>( ptr ) != nullptr ;
-            }
-
-            template< typename T >
-            static T * safe_cast( this_ptr_t ptr ) noexcept
-            {
-                return dynamic_cast< T* >( ptr ) ;
-            }
-
-            template< typename T >
-            static T * just_cast( this_ptr_t ptr ) noexcept
-            {
-                return static_cast< T* >( ptr ) ;
-            }
-        };
-        motor_typedef( node ) ;
+        return this_t::borrow_component< T >() != nullptr;
     }
-}
+
+    template < typename T >
+    T * borrow_component( void_t ) const noexcept
+    {
+        for( auto * comp : _components )
+        {
+            if( auto * ret = dynamic_cast< T * >( comp ); ret != nullptr )
+            {
+                return ret;
+            }
+        }
+        return nullptr;
+    }
+
+    template < typename T >
+    bool_t borrow_component( T *& ret ) const noexcept
+    {
+        for( auto * comp : _components )
+        {
+            if( ret = dynamic_cast< T * >( comp ); ret != nullptr )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    template < typename T >
+    bool_t add_component( motor::core::mtr_safe< T > comp ) noexcept
+    {
+        auto iter =
+            std::find_if( _components.begin(), _components.end(), [ & ]( motor::scene::icomponent_ptr_t comp_in )
+        { return comp_in == comp || ( dynamic_cast< T * >( comp_in ) != nullptr ); } );
+
+        if( iter != _components.end() )
+        {
+            motor::release( comp );
+            return false;
+        }
+
+        comp->attached_to( this );
+        _components.emplace_back( motor::move( comp ) );
+
+        return true;
+    }
+
+    template < typename T >
+    bool_t remove_component( void_t ) noexcept
+    {
+        auto iter =
+            std::find_if( _components.begin(), _components.end(), [ & ]( motor::scene::icomponent_ptr_t comp_in )
+        { return ( dynamic_cast< T * >( comp_in ) != nullptr ); } );
+
+        if( iter == _components.end() )
+        {
+            return false;
+        }
+
+        {
+            auto * comp = *iter;
+            comp->detached();
+            motor::release( comp ) ;
+        }
+
+        _components.erase( iter );
+
+        return true;
+    }
+
+    using for_each_comp_funk_t = std::function< void_t( motor::scene::icomponent_ptr_t ) >;
+    void_t for_each_component( for_each_comp_funk_t funk ) noexcept
+    {
+        for( auto * comp : _components )
+        {
+            funk( comp );
+        }
+    }
+
+  public:
+
+    // helps traversing the tree
+    // use this call to access the apply() function.
+    class MOTOR_SCENE_API traverser
+    {
+      private:
+
+        node_ptr_t _traverse;
+
+      public:
+
+        traverser( node_ptr_t begin ) noexcept;
+        void_t apply( motor::scene::ivisitor_ptr_t ) noexcept;
+    };
+
+    // for nodes to be called
+    class derived_apply
+    {
+      private:
+
+        node_ptr_t _traverse;
+
+      public:
+
+        derived_apply( node_ptr_t begin ) noexcept;
+        motor::scene::result apply( motor::scene::ivisitor_ptr_t ) noexcept;
+    };
+
+  protected:
+
+    virtual motor::scene::result apply( motor::scene::ivisitor_ptr_t ) noexcept = 0;
+
+  public:
+
+    virtual bool_t inputs( motor::wire::inputs_out_t ) noexcept
+    {
+        return false;
+    }
+
+    template < typename T >
+    static bool_t is_of( this_ptr_t ptr ) noexcept
+    {
+        return dynamic_cast< T * >( ptr ) != nullptr;
+    }
+
+    template < typename T >
+    static T * safe_cast( this_ptr_t ptr ) noexcept
+    {
+        return dynamic_cast< T * >( ptr );
+    }
+
+    template < typename T >
+    static T * just_cast( this_ptr_t ptr ) noexcept
+    {
+        return static_cast< T * >( ptr );
+    }
+};
+motor_typedef( node );
+} // namespace scene
+} // namespace motor
