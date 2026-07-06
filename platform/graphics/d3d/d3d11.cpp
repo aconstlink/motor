a0f0e6d7f34fecec79258432a451e07ad47bab18
aconstlink <aconstlink@gmail.com>
2026-07-06 13:19:16 +0200

    :art: missing default value types. more are missing though.

--- platform/graphics/d3d/d3d11.cpp
+++ platform/graphics/d3d/d3d11.cpp
@@ -2356,6 +2356,17 @@ public: // functions
                             vs->data_variable<motor::math::vec3f_t>( var_.name )->set( gdv->get() ) ;
                         }
                     }
+                    else if ( dynamic_cast<motor::msl::generic_default_value< motor::math::vec4f_t >*> ( df ) != nullptr )
+                    {
+                        using ptr_t = motor::msl::generic_default_value< motor::math::vec4f_t > * ;
+                        ptr_t gdv = static_cast<ptr_t>( df ) ;
+                        for ( auto & vs : obj.borrow_varibale_sets() )
+                        {
+                            // for @overwrite specifier
+                            //if( vs->has_data_variable( var_.name ) ) continue ;
+                            vs->data_variable<motor::math::vec4f_t>( var_.name )->set( gdv->get() ) ;
+                        }
+                    }
                     else if ( dynamic_cast<motor::msl::texture_dv_ptr_t> ( df ) != nullptr )
                     {
                         using ptr_t = motor::msl::texture_dv_ptr_t ;
