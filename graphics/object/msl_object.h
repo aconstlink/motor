e11e2d2df360ca0529d04ef9834f81c9db14b90e
aconstlink <aconstlink@gmail.com>
2026-08-13 07:04:18 +0200

    :bulb: applied clang format

--- graphics/object/msl_object.h
+++ graphics/object/msl_object.h
@@ -8,109 +8,122 @@
 
 namespace motor
 {
-    namespace graphics
+namespace graphics
+{
+enum class msl_api_type
+{
+    none,
+    msl_4_0 // gen 4 shader and library
+};
+
+class MOTOR_GRAPHICS_API msl_object : public object
+{
+    motor_this_typedefs( msl_object );
+
+  private:
+
+    // msl shader itself
+    // render object (variables, render states)
+    // shader object (generated shaders and i/o)
+    // geometry object maybe
+
+    motor::string_t _name;
+
+    struct geometry_link
     {
-        enum class msl_api_type
-        {
-            none,
-            msl_4_0         // gen 4 shader and library
-        };
+        motor::string_t name;
+    };
+    motor_typedef( geometry_link );
 
-        class MOTOR_GRAPHICS_API msl_object : public object
-        {
-            motor_this_typedefs( msl_object ) ;
+    motor::vector< geometry_link_t > _geo;
+    motor::vector< motor::string_t > _soo;
 
-        private:
+    struct data
+    {
+        msl_api_type t;
+        motor::string_t code;
+    };
 
-            // msl shader itself
-            // render object (variables, render states)
-            // shader object (generated shaders and i/o)
-            // geometry object maybe
+    motor::vector< data > _datas;
+    motor::vector< motor::graphics::variable_set_mtr_t > _vars;
 
-            motor::string_t _name ;
+    // default compilation listener
+    motor::graphics::compilation_listener_mtr_t _comp_lst =
+        motor::shared( motor::graphics::compilation_listener(), "comp listener" );
 
-            motor::vector< motor::string_t > _geo ;
-            motor::vector< motor::string_t > _soo ;
+  private:
 
-            struct data
-            {
-                msl_api_type t ;
-                motor::string_t code ;
-            };
+    motor::concurrent::mrsw_t _mtx_compilation_listeners;
+    motor::vector< motor::graphics::compilation_listener_mtr_t > _compilation_listeners;
 
-            motor::vector< data > _datas ;
-            motor::vector< motor::graphics::variable_set_mtr_t > _vars ;
+  public:
 
-            // default compilation listener
-            motor::graphics::compilation_listener_mtr_t _comp_lst = 
-                motor::shared( motor::graphics::compilation_listener(), "comp listener" ) ;
+    msl_object( void_t ) noexcept;
+    msl_object( motor::string_in_t name ) noexcept;
+    msl_object( motor::string_in_t name, bool_t const managed ) noexcept;
+    msl_object( this_rref_t rhv ) noexcept;
+    msl_object( this_cref_t rhv ) noexcept;
+    virtual ~msl_object( void_t ) noexcept;
 
-        private:
+  public: // operator
 
-            motor::concurrent::mrsw_t _mtx_compilation_listeners ;
-            motor::vector< motor::graphics::compilation_listener_mtr_t > _compilation_listeners ;
+    this_ref_t operator=( this_cref_t rhv ) noexcept;
+    this_ref_t operator=( this_rref_t rhv ) noexcept;
 
-        public:
+  public:
 
-            msl_object( void_t ) noexcept ;
-            msl_object( motor::string_in_t name ) noexcept ;
-            msl_object( motor::string_in_t name, bool_t const managed ) noexcept ;
-            msl_object( this_rref_t rhv ) noexcept ;
-            msl_object( this_cref_t rhv ) noexcept  ;
-            virtual ~msl_object( void_t ) noexcept ;
+    motor::string_cref_t name( void_t ) const noexcept;
+    this_ref_t add( motor::graphics::msl_api_type const t, motor::string_in_t msl_code ) noexcept;
+    this_ref_t clear_shaders( void_t ) noexcept;
 
-        public: // operator 
+    typedef std::function< void_t( motor::string_in_t ) > foreach_funk_t;
+    void_t for_each_msl(
+        motor::graphics::msl_api_type const t, foreach_funk_t funk ) const noexcept;
 
-            this_ref_t operator = ( this_cref_t rhv ) noexcept ;
-            this_ref_t operator = ( this_rref_t rhv ) noexcept ;
+    size_t link_geometry( motor::string_cref_t name ) noexcept;
+    this_ref_t link_geometry( std::initializer_list< motor::string_t > const & names ) noexcept;
 
-        public:
+    // link to stream out object so geometry can be fed from there.
+    // the geometry is then mainly used for geometry layout.
+    this_ref_t link_geometry( motor::string_cref_t name, motor::string_cref_t soo_name ) noexcept;
 
-            motor::string_cref_t name( void_t ) const noexcept;
-            this_ref_t add( motor::graphics::msl_api_type const t, motor::string_in_t msl_code ) noexcept ;
-            this_ref_t clear_shaders( void_t ) noexcept ;
+    motor::vector< motor::string_t > const & get_geometry( void_t ) const noexcept;
+    motor::vector< motor::string_t > const & get_streamout( void_t ) const noexcept;
 
-            typedef std::function< void_t ( motor::string_in_t ) > foreach_funk_t ;
-            void_t for_each_msl( motor::graphics::msl_api_type const t, foreach_funk_t funk ) const noexcept ;
+    this_t light_clone( motor::string_in_t name ) const noexcept;
 
-            size_t link_geometry( motor::string_cref_t name ) noexcept ;
-            this_ref_t link_geometry( std::initializer_list< motor::string_t > const & names ) noexcept ;
+  public: // variable sets
 
-            // link to stream out object so geometry can be fed from there.
-            // the geometry is then mainly used for geometry layout.
-            this_ref_t link_geometry( motor::string_cref_t name, motor::string_cref_t soo_name ) noexcept ;
+    size_t add_variable_set( motor::graphics::variable_set_mtr_safe_t vs ) noexcept;
+    this_ref_t fill_variable_sets( size_t const ) noexcept;
+    motor::graphics::variable_set_mtr_safe_t get_varibale_set( size_t const id ) noexcept;
+    motor::graphics::variable_set_mtr_t borrow_varibale_set( size_t const id ) const noexcept;
+    motor::vector< motor::graphics::variable_set_mtr_safe_t > get_varibale_sets(
+        void_t ) const noexcept;
+    motor::vector< motor::graphics::variable_set_borrow_t::mtr_t > & borrow_varibale_sets(
+        void_t ) noexcept;
+    motor::vector< motor::graphics::variable_set_borrow_t::mtr_t > const & borrow_varibale_sets(
+        void_t ) const noexcept;
 
-            motor::vector< motor::string_t > const & get_geometry( void_t ) const noexcept ;
-            motor::vector< motor::string_t > const & get_streamout( void_t ) const noexcept ;
+    typedef std::function< void_t( size_t const i, motor::graphics::variable_set_mtr_t ) >
+        for_each_var_funk_t;
+    void_t for_each( for_each_var_funk_t funk ) noexcept;
 
-            this_t light_clone( motor::string_in_t name ) const noexcept ;
+  public:
 
-        public: // variable sets
+    void_t register_listener( motor::graphics::compilation_listener_mtr_safe_t lst ) noexcept;
+    void_t unregister_listener( motor::graphics::compilation_listener_mtr_t lst ) noexcept;
 
-            size_t add_variable_set( motor::graphics::variable_set_mtr_safe_t vs ) noexcept ;
-            this_ref_t fill_variable_sets( size_t const ) noexcept ;
-            motor::graphics::variable_set_mtr_safe_t get_varibale_set( size_t const id ) noexcept ;
-            motor::graphics::variable_set_mtr_t borrow_varibale_set( size_t const id ) const noexcept ;
-            motor::vector< motor::graphics::variable_set_mtr_safe_t > get_varibale_sets( void_t ) const noexcept ;
-            motor::vector< motor::graphics::variable_set_borrow_t::mtr_t > & borrow_varibale_sets( void_t ) noexcept ;
-            motor::vector< motor::graphics::variable_set_borrow_t::mtr_t > const & borrow_varibale_sets( void_t ) const noexcept ;
+    using for_each_change_listerner_funk_t =
+        std::function< void_t( motor::graphics::compilation_listener_mtr_t ) >;
+    void_t for_each( for_each_change_listerner_funk_t f ) noexcept;
 
-            typedef std::function< void_t ( size_t const i, motor::graphics::variable_set_mtr_t ) > for_each_var_funk_t ;
-            void_t for_each( for_each_var_funk_t funk ) noexcept ;
-
-        public:
-
-            void_t register_listener( motor::graphics::compilation_listener_mtr_safe_t lst ) noexcept ;
-            void_t unregister_listener( motor::graphics::compilation_listener_mtr_t lst ) noexcept ;
-            
-            using for_each_change_listerner_funk_t = std::function< void_t ( motor::graphics::compilation_listener_mtr_t ) > ;
-            void_t for_each( for_each_change_listerner_funk_t f ) noexcept ;
-
-            motor::graphics::compilation_listener_mtr_t borrow_compilation_listener( void_t ) const noexcept ;
-            bool_t has_shader_changed( void_t ) const noexcept ;
-            bool_t get_if_successful( motor::graphics::shader_bindings_out_t sb ) noexcept ;
-            bool_t reset_and_successful( motor::graphics::shader_bindings_out_t sb ) noexcept ;
-        };
-        motor_typedef( msl_object ) ;
-    }
-}
+    motor::graphics::compilation_listener_mtr_t borrow_compilation_listener(
+        void_t ) const noexcept;
+    bool_t has_shader_changed( void_t ) const noexcept;
+    bool_t get_if_successful( motor::graphics::shader_bindings_out_t sb ) noexcept;
+    bool_t reset_and_successful( motor::graphics::shader_bindings_out_t sb ) noexcept;
+};
+motor_typedef( msl_object );
+} // namespace graphics
+} // namespace motor
